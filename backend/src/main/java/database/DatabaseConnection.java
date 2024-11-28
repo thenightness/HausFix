e9872b27439625adbb364d04bfa33efb734aadae
Dylan <tru0001p@belgium.fhm.de>
2024-11-28 13:01:38 +0100

    - minor fix, removed the file suffix for customers in SQL statements

--- backend/src/main/java/database/DatabaseConnection.java
+++ backend/src/main/java/database/DatabaseConnection.java
@@ -14,15 +14,15 @@ import java.sql.Statement;
 public class DatabaseConnection implements IDatabaseConnection {
     @Override
     public void createAllTables() {
-        MySQL.executeStatement("CREATE TABLE IF NOT EXISTS customers.sql (id CHAR(36) NOT NULL PRIMARY KEY, firstName VARCHAR(255) NOT NULL, lastName VARCHAR(255) NOT NULL, birthDate DATE NOT NULL, gender ENUM('M', 'W', 'D', 'U') NOT NULL);", null);
-        MySQL.executeStatement("CREATE TABLE IF NOT EXISTS readings (id CHAR(36) NOT NULL PRIMARY KEY, customerId CHAR(36), meterId VARCHAR(255) NOT NULL, meterCount DOUBLE NOT NULL, dateOfReading DATE NOT NULL, kindOfMeter ENUM('HEIZUNG', 'STROM', 'WASSER', 'UNBEKANNT') NOT NULL, comment TEXT, substitute BOOLEAN DEFAULT FALSE, FOREIGN KEY (customerId) REFERENCES customers.sql(id) ON DELETE SET NULL);", null);
+        MySQL.executeStatement("CREATE TABLE IF NOT EXISTS customers (id CHAR(36) NOT NULL PRIMARY KEY, firstName VARCHAR(255) NOT NULL, lastName VARCHAR(255) NOT NULL, birthDate DATE NOT NULL, gender ENUM('M', 'W', 'D', 'U') NOT NULL);", null);
+        MySQL.executeStatement("CREATE TABLE IF NOT EXISTS readings (id CHAR(36) NOT NULL PRIMARY KEY, customerId CHAR(36), meterId VARCHAR(255) NOT NULL, meterCount DOUBLE NOT NULL, dateOfReading DATE NOT NULL, kindOfMeter ENUM('HEIZUNG', 'STROM', 'WASSER', 'UNBEKANNT') NOT NULL, comment TEXT, substitute BOOLEAN DEFAULT FALSE, FOREIGN KEY (customerId) REFERENCES customers(id) ON DELETE SET NULL);", null);
         System.out.println("Tabellen erfolgreich erstellt!");
     }
 
     @Override
     public void truncateTables() {
         MySQL.executeStatement("SET FOREIGN_KEY_CHECKS = 0;", null);
-        MySQL.executeStatement("TRUNCATE TABLE customers.sql;", null);
+        MySQL.executeStatement("TRUNCATE TABLE customers;", null);
         MySQL.executeStatement("TRUNCATE TABLE readings;", null);
         MySQL.executeStatement("SET FOREIGN_KEY_CHECKS = 1;", null);
         System.out.println("Tabellen erfolgreich geleert!");
@@ -31,7 +31,7 @@ public class DatabaseConnection implements IDatabaseConnection {
     @Override
     public void removeAllTables() {
         MySQL.executeStatement("SET FOREIGN_KEY_CHECKS = 0;", null);
-        MySQL.executeStatement("DROP TABLE IF EXISTS customers.sql;", null);
+        MySQL.executeStatement("DROP TABLE IF EXISTS customers;", null);
         MySQL.executeStatement("DROP TABLE IF EXISTS readings;", null);
         MySQL.executeStatement("SET FOREIGN_KEY_CHECKS = 1;", null);
         System.out.println("Tabellen erfolgreich gelöscht!");
