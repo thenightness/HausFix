e98682b71cd63f33684e947d5cbd79ce92aac936
thenightness <marinus.baumeister@gmail.com>
2025-03-21 09:48:17 +0100

    fixes merge to main

---
+++ frontend/src/routes/customer/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	import { Button } from "$lib/components/ui/button/index.js";
+</script>
+
+<Button>
+    jefffdsanjfdsla
+    fdsjkfjlds
+</Button>
+
+<Button>
+    hiidjfnkmashjfdhslakjhfdjksa
+</Button>
+
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,18 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import { enhancedImages } from '@sveltejs/enhanced-img';
 
 export default defineConfig({
-	plugins: [sveltekit(), enhancedImages()],
-	server: {
-		port: 1420,
-		strictPort: true,
-		hmr: {
-			protocol: 'ws',
-			port: 1420
-		}
-	},
-	preview: {
-		port: 1420
-	}
+	plugins: [sveltekit()]
 });
