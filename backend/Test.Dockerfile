58accd269ee8c792be4e2e723f174ef4f258a6f4
thenightness <147348310+thenightness@users.noreply.github.com>
2025-03-22 19:01:24 +0100

    test components implemented

---
+++ backend/Test.Dockerfile
@@ -0,0 +1,2 @@
+FROM jelastic/maven:3.9.9-openjdk-21.0.2-almalinux-9 AS build
+WORKDIR /app
---
+++ backend/src/test/java/customers/CustomerControllerTest.java
@@ -0,0 +1,7 @@
+package customers;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomerControllerTest {
+
+}
