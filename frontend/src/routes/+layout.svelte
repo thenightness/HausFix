39a7837b5ebe0c5162747f0c31cb020ce0d0b92a
thenightness <147348310+thenightness@users.noreply.github.com>
2025-03-23 20:54:28 +0100

    automatic darkmode

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,13 +1,17 @@
 <script lang="ts">
+	import { ModeWatcher } from "mode-watcher";
+
 	import * as Sidebar from "$lib/components/ui/sidebar/index.js";
 	import AppSidebar from "$lib/components/app-sidebar.svelte";
 	import '../app.css';
 	let { children } = $props();
 </script>
 
+<ModeWatcher />
+
 <Sidebar.Provider>
 	<AppSidebar />
-	<main>
-	{@render children?.()}
+	<main class="flex h-full w-full">
+		{@render children?.()}
 	</main>
 </Sidebar.Provider>
