9213f09dfdfccd39f4bfa2a1bd29bd977417fbe6
thenightness <147348310+thenightness@users.noreply.github.com>
2025-05-14 00:54:59 +0200

    add upload and download button

--- frontend/src/lib/components/app-sidebar.svelte
+++ frontend/src/lib/components/app-sidebar.svelte
@@ -1,57 +1,71 @@
 <script lang="ts">
-    import Home from "@lucide/svelte/icons/house";
-    import Customer from "@lucide/svelte/icons/book-user";
-    import Reading from "@lucide/svelte/icons/table-properties";
-    import * as Sidebar from "$lib/components/ui/sidebar/index.js";
+	import Home from '@lucide/svelte/icons/house';
+	import Customer from '@lucide/svelte/icons/book-user';
+	import Reading from '@lucide/svelte/icons/table-properties';
+	import Upload from '@lucide/svelte/icons/upload';
+	import Download from '@lucide/svelte/icons/download';
+	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
+	import Button from './ui/button/button.svelte';
 
-    const items = [
-        {
-            title: "Dashboard",
-            url: "/",
-            icon: Home,
-        },
-        {
-            title: "Kunden",
-            url: "customer",
-            icon: Customer,
-        },
-        {
-            title: "Ablesungen",
-            url: "reading",
-            icon: Reading,
-        },
-    ];
+	const items = [
+		{
+			title: 'Dashboard',
+			url: '/',
+			icon: Home
+		},
+		{
+			title: 'Kunden',
+			url: 'customer',
+			icon: Customer
+		},
+		{
+			title: 'Ablesungen',
+			url: 'reading',
+			icon: Reading
+		}
+	];
+	const option = [
+		{
+			upload: Upload,
+			download: Download
+		}
+	];
 </script>
-    
+
 <Sidebar.Root collapsible="icon">
-    <Sidebar.Header class="group-data-[collapsible=icon]:hidden">
-        <Sidebar.Menu>
-            <Sidebar.MenuItem>
-                <span>HausFix</span>
-            </Sidebar.MenuItem>
-        </Sidebar.Menu>
-    </Sidebar.Header>
-    <Sidebar.Content>
-        <Sidebar.Group>
-            <Sidebar.GroupContent>
-            <Sidebar.Menu>
-                {#each items as item (item.title)}
-                <Sidebar.MenuItem>
-                    <Sidebar.MenuButton>
-                    {#snippet child({ props })}
-                        <a href={item.url} {...props}>
-                        <item.icon/>
-                        <span>{item.title}</span>
-                        </a>
-                    {/snippet}
-                    </Sidebar.MenuButton>
-                </Sidebar.MenuItem>
-                {/each}
-            </Sidebar.Menu>
-            </Sidebar.GroupContent>
-        </Sidebar.Group>
-        </Sidebar.Content>
-    <Sidebar.Footer class="flex">
-        <Sidebar.Trigger class="ml-auto"/> <!--Margin left auto-->
-    </Sidebar.Footer>
+	<Sidebar.Header class="group-data-[collapsible=icon]:hidden">
+		<Sidebar.Menu>
+			<Sidebar.MenuItem>
+				<span>HausFix</span>
+			</Sidebar.MenuItem>
+		</Sidebar.Menu>
+	</Sidebar.Header>
+	<Sidebar.Content>
+		<Sidebar.Group>
+			<Sidebar.GroupContent>
+				<Sidebar.Menu>
+					{#each items as item (item.title)}
+						<Sidebar.MenuItem>
+							<Sidebar.MenuButton>
+								{#snippet child({ props })}
+									<a href={item.url} {...props}>
+										<item.icon />
+										<span>{item.title}</span>
+									</a>
+								{/snippet}
+							</Sidebar.MenuButton>
+							{#if item.title !== 'Dashboard'}
+                                <Button class="float-right w-24 mx-2" href={item.url}><Download /></Button>
+								<Button class="float-right w-24 mx-2" href={item.url}><Upload /></Button>
+							{/if}
+						</Sidebar.MenuItem>
+					{/each}
+				</Sidebar.Menu>
+			</Sidebar.GroupContent>
+		</Sidebar.Group>
+	</Sidebar.Content>
+	<Sidebar.Footer class="flex">
+		<Sidebar.Trigger class="ml-auto" />
+		<!--Margin left auto-->
+	</Sidebar.Footer>
 </Sidebar.Root>
